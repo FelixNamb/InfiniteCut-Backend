5740914faa9ec734d631d440d87f9998c6e80daf
Felix <f.nambotin@orange.fr>
2024-08-15 00:02:15 +0200

    Remise en place des routes et récupération des routes de Sami afin de parvenir à un bon rendu

--- routes/rdv.js
+++ routes/rdv.js
@@ -34,19 +34,65 @@ router.post("/", (req, res) => {
   });
 });
 
-// router.put("/", (req, res) => {
-//   Rdv.updateOne({ _id: req.body.id }, { $set: { date: req.body.date } }).then(
-//     (data) => {
-//       if (data) {
-//         Rdv.findOne({ _id: req.body.id }).then(res.json({ result: true }));
-//       } else {
-//         res.json({
-//           result: false,
-//           error: "Aucune modification effectuée",
-//         });
-//       }
+// router.delete("/", (req,res) => {
+//   if (!checkBody(req.body, ["date", "ObjectId", "plageHoraire"])) {
+//     res.json({ result: false, error: "Veuillez saisir votre retour" });
+//     return;
+//   }
+//   Rdv.deleteOne({date: req.body.date, plageHoraire: req.body.plageHoraire, userPro: req.body.ObjectId})
+//   .then(data => {
+//     if(data.acknowledged){
+//       res.json({message: "bien éffacé"});
+//     } else {
+//       res.json({error: "rien à éffacé"});
 //     }
-//   );
+//   })
 // });
+
+router.delete("/", (req, res) => {
+  console.log(req.body);
+
+  if (!checkBody(req.body, ["date", "ObjectId", "plageHoraire"])) {
+    res.json({ result: false, error: "Aucun rendez-vous trouvé" });
+    return;
+  }
+
+  // Recherche d'abord si l'entrée existe
+  Rdv.findOne({
+    date: req.body.date,
+    plageHoraire: req.body.plageHoraire,
+    userPro: req.body.ObjectId,
+  })
+    .then((data) => {
+      if (data) {
+        console.log("Entrée trouvée :", data);
+        // Si l'entrée existe, on passe à la suppression
+        return Rdv.deleteOne({
+          date: req.body.date,
+          plageHoraire: req.body.plageHoraire,
+          userPro: req.body.ObjectId,
+        });
+      } else {
+        res.json({ error: "Aucun rendez-vous correspondant trouvé à effacer" });
+        throw new Error("Entrée non trouvée"); // Arrête le processus si aucune entrée trouvée
+      }
+    })
+    .then((deleteResult) => {
+      if (deleteResult && deleteResult.deletedCount > 0) {
+        res.json({ message: "Rendez-vous bien effacé" });
+      } else {
+        res.json({ error: "Aucun rendez-vous trouvé à effacer" });
+      }
+    })
+    .catch((err) => {
+      if (err.message !== "Entrée non trouvée") {
+        res.json({
+          result: false,
+          error: "Erreur lors de la suppression",
+          details: err,
+        });
+      }
+    });
+});
 
 module.exports = router;
--- routes/userPro.js
+++ routes/userPro.js
@@ -7,44 +7,40 @@ const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
 
 router.post("/signup", (req, res) => {
-  console.log(req.body);
-  const { prenom, nom, codePostal, mobile, email } = req.body; //clean code pour avoir des variables égales au req.body
+  const { prenom, nom, mobile, email, adresse, nomEnseigne } = req.body; //clean code pour avoir des variables égales au req.body
   if (
-    !checkBody(req.body, ["prenom", "nom", "codePostal", "mobile", "email"])
+    !checkBody(req.body, ["prenom", "nom", "adresse", "mobile", "email", "nomEnseigne"])
   ) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
-  UserPro.findOne({ codePostal, mobile, email }).then((data) => {
+  UserPro.findOne({ nomEnseigne, mobile, email }).then((data) => {
     if (!data) {
       const newUserPro = new UserPro({
         prenom,
         nom,
-        codePostal,
         mobile,
         email,
-        adresse: "",
+        adresse,
         token: uid2(32),
         motDePasse: "",
         mesRdv: [],
         mesNotes: [],
         noteGlobale: 0,
         formules: [],
-        vente: false,
-        image: null,
+        nomEnseigne,
       });
 
       newUserPro.save().then((data) => {
         res.json({ result: true, token: data.token });
+      })
+      .catch((error) => {
+        // Gestion des erreurs
+        console.error("Erreur lors de la création de l'utilisateur:", error);
+        res.status(500).json({ result: false, error: "Internal server error" });
       });
-    } else {
-      res.json({
-        result: false,
-        message: "Ce compte existe déjà. Voulez vous vous connecter ?",
-      });
-    }
-  });
+    }})
 });
 
 router.post("/signin", (req, res) => {
@@ -65,7 +61,7 @@ router.post("/signin", (req, res) => {
 router.get("/", (req, res) => {
   UserPro.find({})
     .populate("notes")
-    .populate("rdv")
+    .populate("rdvs")
     .populate("formules")
     .then((data) => {
       res.json({ result: true, users: data });
@@ -120,6 +116,23 @@ router.get("/:token", (req,res) => {
       res.json({result: false});
     }
   })
-})
+});
+
+router.get("/userpro/:nomEnseigne", (req, res) => {
+  const nomEnseigne = req.params.nomEnseigne;
+  UserPro.findOne({ nomEnseigne: new RegExp(`^${nomEnseigne}$`, "i") })
+    .then((data) => {
+      if (data) {
+        console.log("Je passe ici")
+        res.json({ result: true, user: data });
+      } else {
+        console.log("Je passe là")
+        res.json({ result: false, error: "Nom d'enseigne non valide" });
+      }
+    })
+    .catch((error) => {
+      res.json({ result: false, error: "Erreur lors de la recherche" });
+    });
+});
 
 module.exports = router;
